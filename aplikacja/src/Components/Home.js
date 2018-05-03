d81ee232236e98f3055df3f77824058dacc12f69
Jaga219 <jagodademska@gmail.com>
2018-05-03 15:54:07 +0200

    missing files

--- aplikacja/src/Components/Home.js
+++ aplikacja/src/Components/Home.js
@@ -10,9 +10,9 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        fetch('https://api.themoviedb.org/3/search/movie?api_key=e&language=en-US&query=titanic&page=1&include_adult=false')
+        fetch('https://api.themoviedb.org/3/search/movie?api_key=66eeb56ea4c4c8e8b9edbaaea76e7b7e&language=en-US&query=titanic&page=1&include_adult=false')
             .then((response) => {
-                return response.json();
+                return response.json()
             })
             .then((myJson) => {
                 this.setState({movies: myJson.results});
